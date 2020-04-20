b3fe11fa6d4c76257427bdc72e93c2a7df1408d2
MichaelWojo73 <43526641+MichaelWojo73@users.noreply.github.com>
2020-04-20 21:22:46 +0000

    first day done

---
+++ src/formatters.ts
@@ -0,0 +1,7 @@
+export function formatName(firstName: string, lastName: string) {
+    return lastName + ', ' + firstName
+}
+
+export function addNums(fNum: number, sNum: number) {
+    return fNum + sNum
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,26 @@
+import { formatName, addNums } from "./formatters";
 
-import './styles.css';
-console.log('Ready to Party With Some TypeScript!');
 
+const theButton = document.getElementById('formatIt') as HTMLButtonElement;
+const firstName = document.getElementById('firstName') as HTMLInputElement;
+const lastName = document.getElementById('lastName') as HTMLInputElement;
+const formatted = document.getElementById('formatted') as HTMLHeadingElement;
+
+
+
+console.log({
+    theButton,
+    firstName,
+    lastName,
+    formatted
+});
+
+theButton.addEventListener('click', getNames)
+
+function getNames() {
+    const fName = firstName.value;
+    const lName = lastName.value;
+    console.log({ fName, lName })
+    const fullName = formatName(fName, lName);
+    formatted.innerText = fullName;
+}
